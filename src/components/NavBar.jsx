b27a9be7cf038eab7873d989fd32015a7e892138
MrPoblin <38398131+MrPoblin@users.noreply.github.com>
2024-11-13 14:26:47 +0200

    Add Shopping Cart Icon Counting

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,19 @@
 import { Link } from "react-router-dom";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { useContext } from "react";
+import { CartContext } from "./CartItems";
 
 export default function NavBar(){
+
+    const{cartItems} = useContext(CartContext);
+
+    function getItemCount(total, item){
+        if(isNaN(parseInt(total))){
+            return item.quantity;
+        }
+        return parseInt(total) + item.quantity;
+    }
+
     return(
         <header className="px-8 bg-primary w-full h-20 flex justify-between items-center">
             <div className="size-8"/>
@@ -11,6 +23,7 @@ export default function NavBar(){
             </nav>
             <Link to="/cart/">
                 <ShoppingCartIcon className="size-8 text-background"></ShoppingCartIcon>
+                <div className="absolute top-3 right-5 text-sm bg-background pr-1 pl-1 rounded-full">{cartItems.reduce(getItemCount, '')}</div>
             </Link>
         </header>
     );
